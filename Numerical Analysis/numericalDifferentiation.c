8b3edf5fac9899bd6fa7547fe657fd46cd09cc9a
A. M Samdani Mozumder <114305338+samdani91@users.noreply.github.com>
2023-08-04 16:07:48 +0600

    lab Aug 3

---
+++ Numerical Analysis/numericalDifferentiation.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define PI 3.14159265
+
+int main(int argc,char *argv[])
+{
+
+    double a,b,h;
+    double x[101],y[100],dy[101];
+    int n=100;
+    a=0,b=2*PI;
+    h=(b-a)/n;
+
+    x[0]=a;
+    y[0]=sin(x[0]);
+
+    printf("\"x\",\"f(x)\",\"f'(x)\"\n");
+
+    for(int i=1;i<n;i++){
+        x[i]=x[i-1]+h;
+        y[i]=sin(x[i]);
+    }
+    x[n]=b;
+    y[n]=sin(x[n]);
+
+    dy[0]=(y[1]-y[0])/(x[1]-x[0]);
+
+    for(int i=1;i<n;i++){
+        dy[i]=(y[i+1]-y[i-1])/(x[i+1]-x[i-1]);
+    }
+    dy[n]=(y[n]-y[n-1])/(x[n]-x[n-1]);
+
+    for(int i=0;i<=n;i++){
+        printf("%lf,%lf,%lf\n",x[i],y[i],dy[i]);
+    }
+
+    return 0;
+}
