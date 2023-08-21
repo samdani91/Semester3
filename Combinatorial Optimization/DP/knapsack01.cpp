51f0c9c134affb3669a15d71bc7b102656b2f3fc
A. M Samdani Mozumder <114305338+samdani91@users.noreply.github.com>
2023-08-21 20:14:01 +0600

    first commit

--- Combinatorial Optimization/DP/knapsack01.cpp
+++ Combinatorial Optimization/DP/knapsack01.cpp
@@ -1,24 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int item, knapsack;
+int dp[100][100];
+
 
 int main()
 {
-    freopen("knapsackinput.txt","r",stdin);
-    int item,kanpsack;
-    cin>>item>>kanpsack;
+    freopen("knapsackinput.txt", "r", stdin);
 
-    int price[item],weight[item];
-    int dp[item+1][kanpsack+1];
+    cin >> item >> knapsack;
 
-    for(int i=0;i<item;i++) cin>>weight[i];
-    for(int i=0;i<item;i++) cin>>price[i];
+    int price[item + 1], weight[item + 1];
+    int dp[item + 1][knapsack + 1];
+    int i, w;
 
-    for(int j=0;j<=kanpsack;j++) dp[0][j]=0;
-    for(int i=0;i<=item;i++) dp[i][0]=0;
+    for (i = 1; i <= item; i++)
+        cin >> weight[i];
+    for (i = 1; i <= item; i++)
+        cin >> price[i];
 
-    for(int i=1;i<item;i++){
-        for(int j=1;j<kanpsack;j++){
+    for (w = 0; w <= knapsack; w++)
+        dp[0][w] = 0;
+    for (i = 0; i <= item; i++)
+        dp[i][0] = 0;
+
+    for (i = 1; i <= item; i++)
+    {
+        for (w = 1; w <= knapsack; w++)
+        {
+            if (weight[i] <= w)
+            {
+                dp[i][w] = max(price[i] + dp[i - 1][w - weight[i]], dp[i - 1][w]);
+            }
+            else
+            {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+        // cout<<dp[item][w];
+    }
+    // print(item,knapsack,weight);
+    i = item;
+    int j = knapsack;
+    while (i > 0 && j > 0)
+    {
+        if (dp[i][j] == dp[i - 1][j])
+        {
+            i--;
+            //cout<<i<<j<<endl;
             
+        }
+        else
+        {   
+            cout << "Item: " << i << endl;
+            j = j - weight[i];
+            i--;
         }
     }
 }
