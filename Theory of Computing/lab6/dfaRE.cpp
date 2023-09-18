a8c26d5517dcf847150d6ba3026d25cdf84a51e5
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-18 18:24:09 +0600

    Update dfaRE.cpp

--- Theory of Computing/lab6/dfaRE.cpp
+++ Theory of Computing/lab6/dfaRE.cpp
@@ -1,63 +1,94 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    char transition[3][2];
-    char alphabet[2] = {'0', '1'};
-    freopen("input.txt","r",stdin);
+    int i,j,k;
+    int state,symbol;
+   // freopen("input.txt","r",stdin);
 
-    for (int i = 0; i < 3; i++)
+    cout<<"Enter number of state:\n";
+    cin>>state;
+    cout<<"Enter the states:\n";
+    int states[state];
+    for(i=0; i<state; i++)
     {
-        for (int j = 0; j < 2; j++)
-        {
-            cin>>transition[i][j];
-        }
+        cin>>states[i];
     }
-    
-    map<int, vector<int>> mp;
-
-    vector<int>v(4);
-    
-    for (int j = 0; j < 3; j++)
-    {   
-        for(int s=0;s<v.size();s++) v[s]=-1;
-
-        for (int k = 0; k < 2; k++)
-        {
-            int temp=transition[j][k]-'0';
-            v[temp]=k;
-            
-        }
-
-        cout<<endl;
-        mp.insert(make_pair(j+1,v));
-        
+    cout<<"Enter number of input symbols:\n";
+    cin>>symbol;
+    cout<<"Enter input symbols:\n";
+    string symbols[symbol];
+    for(i=0; i<symbol; i++)
+    {
+        cin>>symbols[i];
     }
 
-    // for(auto x:mp){
-    //     cout<<x.first<<" -> ";
-    //     for(auto ele:x.second){
-    //         cout<<ele<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+    int table[state][symbol];
+    cout<<"Enter transition table:\n";
 
+    for(i=0; i<state; i++)
+    {
+        for(j=0; j<symbol; j++)
+        {
+            cin>>table[i][j];
+        }
+    }
 
-    for(auto x:mp){
-        for(int i=1;i<x.second.size();i++){
-            if(x.first==i && x.second[i]!=-1){
-                cout<<"R("<<x.first<<","<<i<<","<<"0):"<<"eps+"<<x.second[i]<<endl;
-            }
-            else if(x.first==i && x.second[i]==-1){
-                cout<<"R("<<x.first<<","<<i<<","<<"0):"<<"eps"<<endl;
-            }
-            else{
-                cout<<"R("<<x.first<<","<<i<<","<<"0):"<<x.second[i]<<endl;
-            }
+    for(i=0; i<state; i++)
+    {
+        for(j=0; j<symbol; j++)
+        {
+            cout<<table[i][j]<<" ";
         }
         cout<<endl;
     }
 
-    return 0;
+    //main process start
+    string re[3][state+1][state+1];
+
+    for(i=0; i<state; i++)
+    {
+        for(j=i; j<state; j++)
+        {
+            int check=0;
+            for(k=0; k<symbol; k++)
+            {
+                if(table[i][k]==j+1)
+                {
+                    check=1;
+                    re[0][i+1][j+1]+=symbols[k];
+                    if(i==j) re[0][i+1][j+1]+="+E ";
+                }
+
+            }
+            if(check==0) re[0][i+1][j+1]+="N ";
+        }
+    }
+
+    for(k=1; k<3; k++)
+    {
+        for(i=1; i<=state; i++)
+        {
+            for(j=i; j<=state; j++)
+            {
+                re[k][i][j]=re[k-1][i][j]+"+"+re[k-1][i][k]+"("+re[k-1][k][k]+")*"+re[k-1][k][j];
+
+            }
+        }
+
+    }
+
+    cout<<"\nReguler expression:\n\n";
+
+    for(k=0; k<3; k++)
+    {
+        for(i=1; i<=state; i++)
+        {
+            for(j=i; j<=state; j++)
+            {
+                cout<<"R"<<i<<j<<"("<<k<<"): "<<re[k][i][j]<<endl<<endl;
+            }
+        }
+    }
 }
