5a2db1a7bba2062e11622eab92739578e824ed9d
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-11-17 23:17:05 +0600

    Update kruskal.cpp

--- Combinatorial Optimization/Graph Algorithm/kruskal.cpp
+++ Combinatorial Optimization/Graph Algorithm/kruskal.cpp
@@ -4,6 +4,7 @@ using namespace std;
 const int N=1e5+10;
 
 int parent[N];
+int sz[N];
 
 void make(int v);
 int find(int v);
@@ -50,17 +51,20 @@ int main()
 }
 void make(int v){
     parent[v]=v;
+    sz[v]=1;
 }
 
 int find(int v){
     if(v==parent[v]) return v;
-    return find(parent[v]);
+    return parent[v]=find(parent[v]);
 }
 
 void Union(int a,int b){
     a=find(a);
     b=find(b);
     if(a!=b){
+        if(sz[a]<sz[b]) swap(a,b);
         parent[b]=a;
+        sz[a]+=sz[b];
     }
 }
